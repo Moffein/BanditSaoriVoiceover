6bf324671b1e809ce49bcd1a2dd635436891b808
Moffein <22742925+Moffein@users.noreply.github.com>
2023-09-15 16:25:55 -0700

    Add project files.

---
+++ BanditSaoriVoiceover/BanditSaoriVoiceoverPlugin.cs
@@ -0,0 +1,248 @@
+﻿using BanditSaoriVoiceover.Components;
+using BanditSaoriVoiceoverPlugin.Modules;
+using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace BanditSaoriVoiceover
+{
+    [BepInDependency("com.rune580.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency("com.Bread.BanditSaoriSkin", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.Schale.BanditSaoriVoiceover", "BanditSaoriVoiceover", "1.0.0")]
+    public class BanditSaoriVoiceoverPlugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> enableVoicelines;
+        public static bool playedSeasonalVoiceline = false;
+        public static AssetBundle assetBundle;
+        public static SurvivorDef survivorDef = Addressables.LoadAssetAsync<SurvivorDef>("RoR2/Base/Bandit2/Bandit2.asset").WaitForCompletion();
+
+        public void Awake()
+        {
+            Files.PluginInfo = this.Info;
+            BaseVoiceoverComponent.Init();
+            RoR2.RoR2Application.onLoad += OnLoad;
+            new Content().Initialize();
+
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("BanditSaoriVoiceover.banditsaorivoiceoverbundle"))
+            {
+                assetBundle = AssetBundle.LoadFromStream(stream);
+            }
+
+            InitNSE();
+
+            enableVoicelines = base.Config.Bind<bool>(new ConfigDefinition("Settings", "Enable Voicelines"), true, new ConfigDescription("Enable voicelines when using the BanditSaori Skin."));
+            enableVoicelines.SettingChanged += EnableVoicelines_SettingChanged;
+            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions"))
+            {
+                RiskOfOptionsCompat();
+            }
+        }
+
+        private void EnableVoicelines_SettingChanged(object sender, EventArgs e)
+        {
+            RefreshNSE();
+        }
+
+        private void Start()
+        {
+            SoundBanks.Init();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private void RiskOfOptionsCompat()
+        {
+            RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.CheckBoxOption(enableVoicelines));
+            RiskOfOptions.ModSettingsManager.SetModIcon(assetBundle.LoadAsset<Sprite>("texIconSaori"));
+        }
+
+        private void OnLoad()
+        {
+            bool foundSkin = false;
+
+            SkinDef[] skins = SkinCatalog.FindSkinsForBody(BodyCatalog.FindBodyIndex("Bandit2Body"));
+            foreach (SkinDef skinDef in skins)
+            {
+                if (skinDef.name == "SayoriSkin")
+                {
+                    foundSkin = true;
+                    BanditSaoriVoiceoverComponent.requiredSkinDefs.Add(skinDef);
+                    break;
+                }
+            }
+
+            if (!foundSkin)
+            {
+                Debug.LogError("BanditSaoriVoiceover: Bandit Saori SkinDef not found. Voicelines will not work!");
+            }
+            else if (survivorDef)
+            {
+                On.RoR2.CharacterBody.Start += AttachVoiceoverComponent;
+                On.RoR2.SurvivorMannequins.SurvivorMannequinSlotController.RebuildMannequinInstance += (orig, self) =>
+                {
+                    orig(self);
+                    if (self.currentSurvivorDef == survivorDef)
+                    {
+                        //Loadout isn't loaded first time this is called, so we need to manually get it.
+                        //Probably not the most elegant way to resolve this.
+                        if (self.loadoutDirty)
+                        {
+                            if (self.networkUser)
+                            {
+                                self.networkUser.networkLoadout.CopyLoadout(self.currentLoadout);
+                            }
+                        }
+
+                        //Check SkinDef
+                        BodyIndex bodyIndexFromSurvivorIndex = SurvivorCatalog.GetBodyIndexFromSurvivorIndex(self.currentSurvivorDef.survivorIndex);
+                        int skinIndex = (int)self.currentLoadout.bodyLoadoutManager.GetSkinIndex(bodyIndexFromSurvivorIndex);
+                        SkinDef safe = HG.ArrayUtils.GetSafe<SkinDef>(BodyCatalog.GetBodySkins(bodyIndexFromSurvivorIndex), skinIndex);
+                        if (true && enableVoicelines.Value)// && BanditSaoriVoiceoverComponent.requiredSkinDefs.Contains(safe)
+                        {
+                            bool played = false;
+                            if (!playedSeasonalVoiceline)
+                            {
+                                if (System.DateTime.Today.Month == 1 && System.DateTime.Today.Day == 1)
+                                {
+                                    Util.PlaySound("Play_BanditSaori_Lobby_Newyear", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+                                else if (System.DateTime.Today.Month == 9 && System.DateTime.Today.Day == 3)
+                                {
+                                    Util.PlaySound("Play_BanditSaori_Lobby_bday", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+                                else if (System.DateTime.Today.Month == 10 && System.DateTime.Today.Day == 31)
+                                {
+                                    Util.PlaySound("Play_BanditSaori_Lobby_Halloween", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+                                else if (System.DateTime.Today.Month == 12 && System.DateTime.Today.Day == 25)
+                                {
+                                    Util.PlaySound("Play_BanditSaori_Lobby_xmas", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+
+                                if (played)
+                                {
+                                    playedSeasonalVoiceline = true;
+                                }
+                            }
+                            if (!played)
+                            {
+                                if (Util.CheckRoll(5f))
+                                {
+                                    Util.PlaySound("Play_BanditSaori_TitleDrop", self.mannequinInstanceTransform.gameObject);
+                                }
+                                else
+                                {
+                                    Util.PlaySound("Play_BanditSaori_Lobby", self.mannequinInstanceTransform.gameObject);
+                                }
+                            }
+                        }
+                    }
+                };
+            }
+            BanditSaoriVoiceoverComponent.ScepterIndex = ItemCatalog.FindItemIndex("ITEM_ANCIENT_SCEPTER");
+
+            //Add NSE here
+            nseList.Add(new NSEInfo(BanditSaoriVoiceoverComponent.nseShout));
+            nseList.Add(new NSEInfo(BanditSaoriVoiceoverComponent.nseStealth));
+            nseList.Add(new NSEInfo(BanditSaoriVoiceoverComponent.nseBlock));
+            nseList.Add(new NSEInfo(BanditSaoriVoiceoverComponent.nseExLevel));
+            nseList.Add(new NSEInfo(BanditSaoriVoiceoverComponent.nseEx));
+            RefreshNSE();
+        }
+
+        private void AttachVoiceoverComponent(On.RoR2.CharacterBody.orig_Start orig, CharacterBody self)
+        {
+            orig(self);
+            if (self)
+            {
+                if (self.bodyIndex == BodyCatalog.FindBodyIndex("Bandit2Body"))// && (BanditSaoriVoiceoverComponent.requiredSkinDefs.Contains(SkinCatalog.GetBodySkinDef(self.bodyIndex, (int)self.skinIndex)))
+                {
+                    BaseVoiceoverComponent existingVoiceoverComponent = self.GetComponent<BaseVoiceoverComponent>();
+                    if (!existingVoiceoverComponent) self.gameObject.AddComponent<BanditSaoriVoiceoverComponent>();
+                }
+            }
+        }
+
+        private void InitNSE()
+        {
+            BanditSaoriVoiceoverComponent.nseShout = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            BanditSaoriVoiceoverComponent.nseShout.eventName = "Play_BanditSaori_Shout";
+            Content.networkSoundEventDefs.Add(BanditSaoriVoiceoverComponent.nseShout);
+
+            BanditSaoriVoiceoverComponent.nseStealth = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            BanditSaoriVoiceoverComponent.nseStealth.eventName = "Play_BanditSaori_Stealth";
+            Content.networkSoundEventDefs.Add(BanditSaoriVoiceoverComponent.nseStealth);
+
+            BanditSaoriVoiceoverComponent.nseBlock = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            BanditSaoriVoiceoverComponent.nseBlock.eventName = "Play_BanditSaori_Muda";
+            Content.networkSoundEventDefs.Add(BanditSaoriVoiceoverComponent.nseBlock);
+
+            BanditSaoriVoiceoverComponent.nseExLevel = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            BanditSaoriVoiceoverComponent.nseExLevel.eventName = "Play_BanditSaori_ExLevel";
+            Content.networkSoundEventDefs.Add(BanditSaoriVoiceoverComponent.nseExLevel);
+
+            BanditSaoriVoiceoverComponent.nseEx = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            BanditSaoriVoiceoverComponent.nseEx.eventName = "Play_BanditSaori_Ex";
+            Content.networkSoundEventDefs.Add(BanditSaoriVoiceoverComponent.nseEx);
+        }
+
+        public void RefreshNSE()
+        {
+            foreach (NSEInfo nse in nseList)
+            {
+                nse.ValidateParams();
+            }
+        }
+
+        public static List<NSEInfo> nseList = new List<NSEInfo>();
+        public class NSEInfo
+        {
+            public NetworkSoundEventDef nse;
+            public uint akId = 0u;
+            public string eventName = string.Empty;
+
+            public NSEInfo(NetworkSoundEventDef source)
+            {
+                this.nse = source;
+                this.akId = source.akId;
+                this.eventName = source.eventName;
+            }
+
+            private void DisableSound()
+            {
+                nse.akId = 0u;
+                nse.eventName = string.Empty;
+            }
+
+            private void EnableSound()
+            {
+                nse.akId = this.akId;
+                nse.eventName = this.eventName;
+            }
+
+            public void ValidateParams()
+            {
+                if (this.akId == 0u) this.akId = nse.akId;
+                if (this.eventName == string.Empty) this.eventName = nse.eventName;
+
+                if (!enableVoicelines.Value)
+                {
+                    DisableSound();
+                }
+                else
+                {
+                    EnableSound();
+                }
+            }
+        }
+    }
+}
---
+++ BanditSaoriVoiceover/Components/BanditSaoriVoiceoverComponent.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using RoR2;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace BanditSaoriVoiceover.Components
+{
+    public class BanditSaoriVoiceoverComponent : BaseVoiceoverComponent
+    {
+        public static List<SkinDef> requiredSkinDefs = new List<SkinDef>();
+        public static ItemIndex ScepterIndex;
+
+        public static NetworkSoundEventDef nseShout, nseStealth, nseBlock, nseExLevel, nseEx;
+
+        private float levelCooldown = 0f;
+        private float blockedCooldown = 0f;
+        private float lowHealthCooldown = 0f;
+        private float specialCooldown = 0f;
+        private float utilityCooldown = 0f;
+
+        private bool acquiredScepter = false;
+
+        protected override void Awake()
+        {
+            spawnVoicelineDelay = 3f;
+            if (Run.instance && Run.instance.stageClearCount == 0)
+            {
+                spawnVoicelineDelay = 6.5f;
+            }
+            base.Awake();
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            if (inventory && inventory.GetItemCount(ScepterIndex) > 0) acquiredScepter = true;
+        }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            if (specialCooldown > 0f) specialCooldown -= Time.fixedDeltaTime;
+            if (utilityCooldown > 0f) utilityCooldown -= Time.fixedDeltaTime;
+            if (levelCooldown > 0f) levelCooldown -= Time.fixedDeltaTime;
+            if (blockedCooldown > 0f) blockedCooldown -= Time.fixedDeltaTime;
+            if (lowHealthCooldown > 0f) lowHealthCooldown -= Time.fixedDeltaTime;
+        }
+
+        public override void PlayDamageBlockedServer()
+        {
+            if (!NetworkServer.active || blockedCooldown > 0f) return;
+            bool played = TryPlayNetworkSound(nseBlock, 0.55f, false);
+            if (played) blockedCooldown = 30f;
+        }
+
+        public override void PlayDeath()
+        {
+            TryPlaySound("Play_BanditSaori_Defeat", 4f, true);
+        }
+
+        public override void PlayHurt(float percentHPLost)
+        {
+            if (percentHPLost >= 0.1f)
+            {
+                TryPlaySound("Play_BanditSaori_TakeDamage", 0f, false);
+            }
+        }
+
+        public override void PlayJump() { }
+
+        public override void PlayLevelUp()
+        {
+            if (levelCooldown > 0f) return;
+            bool played;
+            if (Util.CheckRoll(50f))
+            {
+                played = TryPlaySound("Play_BanditSaori_LevelUp_Short", 1.35f, false);
+            }
+            else
+            {
+                played = TryPlaySound("Play_BanditSaori_LevelUp_Long", 6.6f, false);
+            }
+            if (played) levelCooldown = 60f;
+        }
+
+        public override void PlayLowHealth()
+        {
+            if (lowHealthCooldown > 0f) return;
+            bool playedSound;
+
+            if (Util.CheckRoll(50f))
+            {
+                playedSound = TryPlaySound("Play_BanditSaori_LowHealth", 0f, false);
+            }
+            else if (Util.CheckRoll(90f))
+            {
+                playedSound = TryPlaySound("Play_BanditSaori_Lobby5", 3.8f, false);
+            }
+            else
+            {
+                if (Util.CheckRoll(50f))
+                {
+                    playedSound = TryPlaySound("Play_BanditSaori_Memorial2", 7.35f, false);
+                }
+                else
+                {
+                    playedSound = TryPlaySound("Play_BanditSaori_Memorial4", 12.8f, false);
+                }
+            }
+                
+            if (playedSound) lowHealthCooldown = 60f;
+        }
+
+        public override void PlayPrimaryAuthority() { }
+
+        public override void PlaySecondaryAuthority()
+        {
+            TryPlayNetworkSound(nseShout, 0f, false);
+        }
+
+        public override void PlaySpawn()
+        {
+            TryPlaySound("Play_BanditSaori_Spawn", 2.4f, true);
+        }
+
+        public override void PlaySpecialAuthority()
+        {
+            if (specialCooldown > 0f) return;
+            bool played;
+
+            if (Util.CheckRoll(80f))
+            {
+                played = TryPlayNetworkSound(nseExLevel, 1.9f, false);
+            }
+            else
+            {
+                played = TryPlayNetworkSound(nseEx, 4.4f, false);
+            }
+
+            if (played) specialCooldown = 8f;
+        }
+
+        public override void PlayTeleporterFinish()
+        {
+            TryPlaySound("Play_BanditSaori_Victory", 2.7f, false);
+        }
+
+        public override void PlayTeleporterStart()
+        {
+            TryPlaySound("Play_BanditSaori_Ex", 4.4f, true);
+        }
+
+        public override void PlayUtilityAuthority()
+        {
+            if (utilityCooldown > 0f) return;
+            bool played = TryPlayNetworkSound(nseStealth, 1.9f, false);
+            if (played) utilityCooldown = 30f;
+        }
+
+        public override void PlayVictory()
+        {
+            //TryPlaySound("Play_BanditSaori_VanitasFull", 2.9f, true);
+            TryPlaySound("Play_BanditSaori_Lobby3", 4.7f, true);
+        }
+
+        protected override void Inventory_onItemAddedClient(ItemIndex itemIndex)
+        {
+            base.Inventory_onItemAddedClient(itemIndex);
+            if (ScepterIndex != ItemIndex.None && itemIndex == ScepterIndex)
+            {
+                PlayAcquireScepter();
+            }
+            else
+            {
+                ItemDef id = ItemCatalog.GetItemDef(itemIndex);
+                if (id == RoR2Content.Items.Squid || id == RoR2Content.Items.Plant || id == RoR2Content.Items.SlowOnHit)
+                {
+                    PlayBadItem();
+                }
+                else if (id && id.deprecatedTier == ItemTier.Tier3)
+                {
+                    PlayAcquireLegendary();
+                }
+            }
+        }
+
+        public void PlayAcquireScepter()
+        {
+            if (acquiredScepter) return;
+            TryPlaySound("Play_BanditSaori_AcquireScepter", 22.75f, true);
+            acquiredScepter = true;
+        }
+        public void PlayBadItem()
+        {
+            if (Util.CheckRoll(50f))
+            {
+                TryPlaySound("Play_BanditSaori_Omoshiroi", 0.75f, false);
+            }
+            else
+            {
+                if (Util.CheckRoll(50f))
+                {
+                    TryPlaySound("Play_BanditSaori_Vanitas", 1.2f, false);
+                }
+                else
+                {
+                    TryPlaySound("Play_BanditSaori_VanitasFull", 2.9f, false);
+                }
+            }
+        }
+
+        public void PlayAcquireLegendary()
+        {
+            if (Util.CheckRoll(75f))
+            {
+                TryPlaySound("Play_BanditSaori_Relationship", 3.7f, false);
+            }
+            else
+            {
+                TryPlaySound("Play_BanditSaori_Relationship_Long", 12.25f, false);
+            }
+        }
+    }
+}
